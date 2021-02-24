fa089098751a712b78be0120bef189f1956e789d
Robertdesenvolvermec <robertsanttos.rs@gmail.com>
2021-02-24 17:55:43 -0300

    Consertar o calculo de saques
    Não continuar a aula antes de fazer esse calculo certo.

--- AluraBanck/alurabanck/bin/alurabanck.dart
+++ AluraBanck/alurabanck/bin/alurabanck.dart
@@ -1,25 +1,35 @@
 void main() {
-  contaCorrente conta = contaCorrente();
-  conta.titula = 'Robert';
-  conta.agencia = 123;
-  conta.conta = 1234;
-  
-  conta.saldo = 0.0;
-  
-  print(conta.saldo);
+  contaCorrente contaDaAmanda = contaCorrente();
+  contaCorrente contaDoThiago = contaCorrente();
+  contaDaAmanda.titula = 'Amanda';
+  contaDoThiago.titula = 'Thiago';
 
-  conta.saldo = 150.0;
+  print('Saldo da ${contaDaAmanda.titula}: ${contaDaAmanda.saldo}');
+  contaDaAmanda.saque(121.0);
+  print('Saldo atual da ${contaDaAmanda.titula}: ${contaDaAmanda.saldo}');
+  print('');
 
-  print(conta.saldo);
-  
-  conta.saldo += 50.0;
-
-  print(conta.saldo);
+  print('Saldo da ${contaDoThiago.titula}: ${contaDoThiago.saldo}');
+  contaDoThiago.saque(121.0);
+  print('Saldo atual da ${contaDoThiago.titula}: ${contaDoThiago.saldo}');
 }
 
 class contaCorrente {
   String titula;
-  int agencia;
+  int agencia = 145;
   int conta;
-  double saldo;
+  double saldo = 20.0;
+
+  void saque(double valorDoSaque) {
+    double limiteDeSaque = (saldo + 100) - valorDoSaque;
+
+    if (this.saldo - valorDoSaque < -100) {
+      print('Sem saldo suficiente');
+      print(
+          'Tendando sacar $valorDoSaque; limite de saque ${limiteDeSaque} reais');
+    } else {
+      print('Sacando $valorDoSaque reais');
+      this.saldo -= valorDoSaque;
+    }
+  }
 }
