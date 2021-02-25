4677f5d92ff94030e3eb39bc1bad9c81be156d6b
Robert dos Santos Silva <60759036+Robertdesenvolvermec@users.noreply.github.com>
2021-02-25 09:44:49 -0300

    Add o cheque especial.

--- AluraBanck/alurabanck/bin/alurabanck.dart
+++ AluraBanck/alurabanck/bin/alurabanck.dart
@@ -2,16 +2,33 @@ void main() {
   contaCorrente contaDaAmanda = contaCorrente();
   contaCorrente contaDoThiago = contaCorrente();
   contaDaAmanda.titula = 'Amanda';
+  contaDaAmanda.chequeEspecial = -110.0;
   contaDoThiago.titula = 'Thiago';
 
-  print('Saldo da ${contaDaAmanda.titula}: ${contaDaAmanda.saldo}');
-  contaDaAmanda.saque(121.0);
+  //contaDaAmanda.deposito(20.0);
+  //print('Saldo da ${contaDaAmanda.titula}: ${contaDaAmanda.saldo}');
+  //contaDaAmanda.saque(80.0);
+  //print('Saldo atual da ${contaDaAmanda.titula}: ${contaDaAmanda.saldo}');
+  //print('');
+
+  //contaDoThiago.deposito(50.0);
+  //print('Saldo da ${contaDoThiago.titula}: ${contaDoThiago.saldo}');
+  //contaDoThiago.saque(130.0);
+  //print('Saldo atual da ${contaDoThiago.titula}: ${contaDoThiago.saldo}');
+
   print('Saldo atual da ${contaDaAmanda.titula}: ${contaDaAmanda.saldo}');
+  print('Saldo atual da ${contaDoThiago.titula}: ${contaDoThiago.saldo}');
+  print('');
+  bool sucesso = contaDaAmanda.transferencia(130.0, contaDoThiago);
+  print(sucesso);
+  print('');
+  print('Saldo atual da ${contaDaAmanda.titula}: ${contaDaAmanda.saldo}');
+  print('Saldo atual da ${contaDoThiago.titula}: ${contaDoThiago.saldo}');
   print('');
 
-  print('Saldo da ${contaDoThiago.titula}: ${contaDoThiago.saldo}');
-  contaDoThiago.saque(121.0);
-  print('Saldo atual da ${contaDoThiago.titula}: ${contaDoThiago.saldo}');
+  contaDaAmanda.deposito(300.0);
+  print('Saldo atual da ${contaDaAmanda.titula}: ${contaDaAmanda.saldo}');
+
 }
 
 class contaCorrente {
@@ -19,17 +36,41 @@ class contaCorrente {
   int agencia = 145;
   int conta;
   double saldo = 20.0;
+  double chequeEspecial = -100.0;
 
-  void saque(double valorDoSaque) {
-    double limiteDeSaque = (saldo + 100) - valorDoSaque;
-
-    if (this.saldo - valorDoSaque < -100) {
+  bool verificaSaldo(double valor){
+    if (this.saldo - valor < chequeEspecial){
       print('Sem saldo suficiente');
-      print(
-          'Tendando sacar $valorDoSaque; limite de saque ${limiteDeSaque} reais');
+      return false;
     } else {
-      print('Sacando $valorDoSaque reais');
-      this.saldo -= valorDoSaque;
+      print('Movimentando $valor reais');
+      return true;
     }
+  }
+
+  bool transferencia(double valorDaTransferencia, contaCorrente contaDestino){
+    if (!verificaSaldo(valorDaTransferencia)){
+      return false;
+    } else {
+      this.saldo -= valorDaTransferencia;
+      contaDestino.deposito(valorDaTransferencia); 
+      return true;
+    }
+  }
+
+  bool saque(double valorDoSaque) {
+
+    if (!verificaSaldo(valorDoSaque)) {
+      return false;
+    } else {
+      this.saldo -= valorDoSaque;
+      return true;
+    }
+  }
+
+  double deposito(double valorDoDeposito){
+    print('Foi feito um deposito na conta');
+    this.saldo += valorDoDeposito;
+    return this.saldo;
   }
 }
